cd05ac81c13889d85b862e0d0fa8203af35d260d
Frederic Suter <frederic.suter@cc.in2p3.fr>
2015-07-08 10:57:40 +0200

    improve code logic (I hope)

--- src/SE.java
+++ src/SE.java
@@ -1,9 +1,36 @@
 import org.simgrid.msg.Msg;
 import org.simgrid.msg.Host;
 import org.simgrid.msg.Process;
+import org.simgrid.msg.Task;
+
 import org.simgrid.msg.MsgException;
 
 public class SE extends GridService {
+
+	private SEMessage getFrom (String mailbox) {
+		SEMessage message = null;
+		try {
+			message = (SEMessage) Task.receive(mailbox);
+			Msg.debug("Received a '" + message.getType().toString() + 
+					"' message from " + mailbox);
+			// Simulate the cost of the local processing of the request.
+			// Depends on the value set when the Message was created
+			message.execute();
+		} catch (MsgException e) {
+			e.printStackTrace();
+		}
+
+		return message;
+	}
+
+	private void sendAckTo (String mailbox) {
+		SEMessage.sendTo(mailbox, SEMessage.Type.UPLOAD_ACK, "", 0);
+		Msg.debug("'SE@"+ getName() +"' sent an ACK on '" + mailbox + "'");
+	}
+
+	public void sendFileTo (String destination, long size) {
+		SEMessage.sendTo(destination, SEMessage.Type.FILE_TRANSFER, null, size);
+	}
 
 	public SE (Host host, String name, String[]args) {
 		super(host,name,args);
@@ -11,50 +38,41 @@ public class SE extends GridService {
 
 	public void main(String[] args) throws MsgException {
 		// Keep track of this process in a global list. This is used at the end
-		// of the simulation to send a FINALIZE message and cleanly stop this 
-		// process. 
+		// of the simulation to cleanly kill this process. 
 		Msg.debug("Register SE on "+ name);
 		VIPSimulator.getSEList().add(this);
 
-		for (int i=0; i<10; i++){
+		for (int i = 0; i < 10; i++){
 			listeners.add(new Process(name, name+"_"+i) {
 				public void main(String[] args) throws MsgException {
 					String mailbox = getName();
 					Msg.debug("Start a new listener on: " + mailbox);
 
 					while (true){
-						SEMessage message = SEMessage.getFrom(mailbox);
+						SEMessage message = getFrom(mailbox);
 						
 						switch(message.getType()){
 						case DOWNLOAD_REQUEST:
-							// A worker asked for a physical file. It is send 
-							// in an asynchronous way so that this listener
-							// doesn't have to wait for the completion of the 
-							// transfer to handle subsequent  messages.
+							// A worker asked for a physical file. A data 
+							// transfer of getSize() bytes occurs upon reply.
 							// TODO This will have to be replaced/completed by 
 							// TODO some I/O operations at some point to 
 							// TODO increase realism.
 							Msg.debug("SE '"+ name + "' send file '" +
-									message.getLogicalName() + "' of size " +
+									message.getFileName() + "' of size " +
 									message.getSize() + " to '" +
-									message.getSenderMailbox() + "'");
-							SEMessage.sendTo("return-"+mailbox, 
-									SEMessage.Type.SEND_FILE,
-									message.getSize());
+									message.getSenderName() + "'");
+							sendFileTo("return-"+mailbox, message.getSize());
 
 							break;
-						case UPLOAD_FILE:
+						case FILE_TRANSFER:
 							// A physical file has been received (inducing a 
-							// data transfer. An ACK is sent back to notify the 
-							// reception of the file.
+							// data transfer). An ACK is sent back to notify  
+							// the reception of the file.
 							// TODO This will have to be replaced/completed by 
 							// TODO some I/O operations at some point to 
 							// TODO increase realism.
-							SEMessage.sendTo("return-"+mailbox, 
-									SEMessage.Type.UPLOAD_ACK);
-
-							Msg.debug("SE '"+ name + "' sent ack back to '" +
-									message.getSenderMailbox() + "'");
+							sendAckTo("return-"+mailbox);
 							break;
 						default:
 							break;
@@ -62,17 +80,15 @@ public class SE extends GridService {
 					}
 				}
 			});
+			listeners.lastElement().start();
 		}
-		for(Process p : listeners)
-			p.start();
 	}
 
 	public void upload (long size) {
 		String mailbox = this.findAvailableMailbox(100);
-		SEMessage.sendTo(mailbox, SEMessage.Type.UPLOAD_FILE, 
-				size);
+		SEMessage.sendTo(mailbox, SEMessage.Type.FILE_TRANSFER, null, size);
 		Msg.info("Sent upload request of size " + size +". Waiting for an ack");
-		SEMessage.getFrom("return-"+mailbox);
+		getFrom("return-"+mailbox);
 	}
 
 	public void download(String fileName, long fileSize){
@@ -81,7 +97,9 @@ public class SE extends GridService {
 				fileName, fileSize);
 		Msg.info("Sent download request for '" + fileName + 
 				"'. Waiting for reception ...");
-		SEMessage.getFrom("return-"+mailbox);
+		getFrom("return-"+mailbox);
 	}
+	
+
 }
 
--- src/SEMessage.java
+++ src/SEMessage.java
@@ -7,94 +7,65 @@ import org.simgrid.msg.TaskCancelledException;
 public class SEMessage extends Task {
 	public enum Type{
 		DOWNLOAD_REQUEST,
-		SEND_FILE,
-		UPLOAD_FILE,
+		FILE_TRANSFER,
 		UPLOAD_ACK
 	};
 
 	private Type type;
-	private String logicalName = null;
+	private String fileName = null;
 	private long size = 0;
 
-	public Type getType() {
-		return type;
-	}
-
-	public String getSenderMailbox(){
-		return getSender().getPID()+ "@" + getSource().getName();
-	}
-
-	public String getLogicalName() {
-		return logicalName;
-	}
-
-	public long getSize() {
-		return size;
-	}
-
-	public SEMessage(Type type, String logicalName, long size) {
+	/**
+	 * Constructor, builds a new control (DOWNLOAD_REQUEST/UPLOAD_ACK) message
+	 */
+	private SEMessage(Type type, String fileName, long size) {
 		super(type.toString(), 1e6, 100);
 		this.type = type;
-		this.logicalName=logicalName;
+		this.fileName=fileName;
 		this.size=size;
 	}
 
 	/**
-	 * Constructor, builds a new UPLOAD_FILE/SEND_FILE message
+	 * Constructor, builds a new FILE_TRANSFER message
 	 */
-	public SEMessage(Type type, long size){
+	private SEMessage(Type type, long size){
 		super(type.toString(), 0, size);
 		this.type = type;
 		this.size = size;
 	}
 
-	public void execute() throws  HostFailureException,TaskCancelledException{
+	public Type getType() {
+		return type;
+	}
+
+	public String getSenderName(){
+		return getSender().getPID()+ "@" + getSource().getName();
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public long getSize() {
+		return size;
+	}
+	
+	public void execute() throws  HostFailureException, TaskCancelledException{
 		super.execute();
 	}
 
-	public static SEMessage getFrom (String mailbox) {
-		SEMessage message = null;
-		try {
-			message = (SEMessage) Task.receive(mailbox);
-			Msg.debug("Received a '" + message.type.toString() + 
-					"' message from " + mailbox);
-			// Simulate the cost of the local processing of the request.
-			// Depends on the value set when the Message was created
-			message.execute();
-		} catch (MsgException e) {
-			e.printStackTrace();
-		}
-
-		return message;
-	}
-
-	public static void sendTo (String destination, Type type, 
-			String logicalName, long size) {
-		SEMessage m = (logicalName == null) ? 
+	public static void sendTo (String mailbox, Type type, String fileName, 
+			long size) {
+		SEMessage message = (fileName == null) ? 
 				new SEMessage (type, size) : 
-				new SEMessage (type, logicalName, size);
+				new SEMessage (type, fileName, size);
 		try{
-			Msg.debug("Send a '" + type.toString() + "' message to " +
-					destination);
-			m.send(destination);
+			Msg.debug("Send a '" + type.toString() + "' message to " + mailbox);
+			message.send(mailbox);
 		} catch (MsgException e) {
 			Msg.error("Something went wrong when emitting a '" + 
-				type.toString() +"' message to '" + destination + "'");
+				type.toString() +"' message to '" + mailbox + "'");
 			e.printStackTrace();
 		}
-	}
-
-	/**
-	 * Specialized send of a UPLOAD_ACK message
-	 */
-	public static void sendTo (String destination, Type type) {
-		sendTo(destination, type, type.toString(), 0);
-	}
-
-	/**
-	 *  Specialized send of a UPLOAD_FILE/SEND_FILE message
-	 */
-	public static void sendTo (String destination, Type type, long size) {
-		sendTo(destination, type, null, size);
 	}
 }
