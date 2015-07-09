2ca880c6e3351f27f29f2b28e2c5e5e1c0a64319
Frederic Suter <frederic.suter@cc.in2p3.fr>
2015-07-09 14:22:45 +0200

    mailbox to name

--- src/Job.java
+++ src/Job.java
@@ -1,55 +1,36 @@
 import org.simgrid.msg.Msg;
-import org.simgrid.msg.MsgException;
 import org.simgrid.msg.Process;
 import org.simgrid.msg.Host;
-import org.simgrid.msg.Task;
 
 public abstract class Job extends Process{
-	private String mailbox;
+	private String name;
 	private SE closeSE;
 
-	protected void setMailbox(){
-		mailbox = Integer.toString(this.getPID()) + "@" +
-				getHost().getName();
+	protected void setName(){
+		name = Integer.toString(this.getPID()) + "@" + getHost().getName();
 	}
 
-	public String getMailbox(){
-		return mailbox;
+	public String getName(){
+		return name;
 	}
 	
 	public SE getCloseSE() {
 		return closeSE;
 	}
 
-	public GateMessage getFrom (String mailbox) {
-		GateMessage message = null;
-		try {
-			message = (GateMessage) Task.receive(mailbox);
-			Msg.debug("Received a '" + message.getType().toString() + 
-					"' message from " + mailbox);
-			// Simulate the cost of the local processing of the request.
-			// Depends on the value set when the Message was created
-			message.execute();
-		} catch (MsgException e) {
-			e.printStackTrace();
-		}
-
-		return message;
-	}
-
 	public void begin(){
-		Msg.debug("Sending a 'BEGIN' message to '" + mailbox +"'");
-		GateMessage.sendTo(mailbox, Message.Type.BEGIN, 0);
+		Msg.debug("Sending a 'BEGIN' message to '" + name +"'");
+		GateMessage.sendTo(name, Message.Type.BEGIN, 0);
 	}
 
 	public void carryOn (){
-		Msg.debug("Sending a 'CARRY_ON' message to '" + mailbox +"'");
-		GateMessage.sendTo(mailbox, Message.Type.CARRY_ON, 0);
+		Msg.debug("Sending a 'CARRY_ON' message to '" + name +"'");
+		GateMessage.sendTo(name, Message.Type.CARRY_ON, 0);
 	}
 
 	public void end(){
-		Msg.debug("Sending a 'END' message to '" + mailbox +"'");
-		GateMessage.sendTo(mailbox, Message.Type.END, 0);
+		Msg.debug("Sending a 'END' message to '" + name +"'");
+		GateMessage.sendTo(name, Message.Type.END, 0);
 	}
 
 	public Job(Host host, String name, String[]args) {
